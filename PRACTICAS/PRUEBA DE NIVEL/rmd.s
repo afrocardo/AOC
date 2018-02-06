0eb089e50c368e50ecc6bde83026257d55da8085
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2018-02-06 13:38:44 +0100

    Update rmd.s

--- PRACTICAS/PRUEBA DE NIVEL/rmd.s
+++ PRACTICAS/PRUEBA DE NIVEL/rmd.s
@@ -10,8 +10,29 @@ MemArray: .word 2,3,5,7,11,113,17,19,23,29,31,37,41,43,47
 
 
 .text 
+.globl main 
 main: 
-
+  li $v0, 5	#Lee un entero desde consola
+	lw $a0, n	#Lo escribe en n
+	syscall
+	
+	lw $s2, n
+	mul $s2, $s2,  4
+	li $s4, 0
+	
+	bucle:
+		lw $s1, MemArray($s4)
+		add $s3, $s3, $s1		
+		addi $s4, $s4, 4	#Incrementamos el índice del array
+		bne $s4, $s2, bucle
+	
+	
+	lw $a0, ($s3)				
+	li $v0, 4 		
+	syscall		
+	
+	li $v0, 10
+	syscall
 
 
 #Uno que lleve los accesos al bucle
