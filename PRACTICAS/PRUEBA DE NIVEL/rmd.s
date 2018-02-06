53886faaf108ed36d6580897193f4463ddf0ce2c
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2018-02-06 13:36:02 +0100

    Add files via upload

---
+++ PRACTICAS/PRUEBA DE NIVEL/rmd.s
@@ -0,0 +1,22 @@
+#Autor: Ricardo Meza
+#Ejercicio 1 prácticas
+#Realizar un programa,que a partir del segmento de texto siguiente,calcule la suma de los n primeros numeros almacenados en el array(utilizando bucle).
+#MIPS no admite acceso no alineados en memoria.
+
+.data 
+
+n: word 10 
+MemArray: .word 2,3,5,7,11,113,17,19,23,29,31,37,41,43,47
+
+
+.text 
+main: 
+
+
+
+#Uno que lleve los accesos al bucle
+
+#Otro que cuente las iteraciones
+
+
+
